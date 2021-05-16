81e9d03aad83d2768136f989e26cb6ce134aa064
Jun Hwang <jhwang2525@gmail.com>
2021-05-16 15:51:49 -0700

    code-quiz

---
+++ assets/questions.js
@@ -0,0 +1,32 @@
+var myQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: 
+           ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: 3
+    },
+    {   
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choices: 
+            ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctAnswer: 3 
+        
+    },
+    {
+        question: "Array in Javascript can be used to store",
+        choices: 
+           ["numbers and strings", "other arrays", "booleans", "All of the above"],
+        correctAnswer: 4
+    },
+    {
+        question: "String values must inclosed within ____ when being assigned to a variable",
+        choices: 
+           ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: 3
+    },{
+        question: "A very useful too used during development and debugging for printing content to the debugger is",
+        choices: 
+           ["JavaScript", "TerminalBash", "for loops", "console.log"],
+        correctAnswer: 2
+    }
+]
